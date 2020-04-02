6819bcb94c3b778efea00dc1c497fdccd220074d
Norberto Santiago <norbertosantiago82@gmail.com>
2020-04-02 21:12:17 +0000

    Automatically backed up by Learn

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,7 +3,7 @@ class Song < ActiveRecord::Base
   belongs_to :genre
 
   def get_genre_name
-    self.genres.map do |genre| #return string of genres 
+    self.genre.map do |genre| #return string of genres 
       genre.name
     end 
   end
